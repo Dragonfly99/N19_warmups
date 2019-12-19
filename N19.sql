1cdee12a7806803e9772c7ce36d7aeb2a9a49139
Kerry <kerrspeed@yahoo.com>
2019-12-19 09:59:37 -0500

    Finished part 1 of this.  Understanding partition better

--- N19.sql
+++ N19.sql
@@ -1,11 +1,16 @@
-With order_info As (SELECT SUM(order_date) as total
-from customers
+with top_3 as (SELECT c.customer_id, country, MAX(order_date) - MIN(order_date) as diff, RANK() OVER(Partition by country order by MAX(order_date) - MIN(order_date)  desc) de_rank
+from customers as c
 join orders
 using (customer_id)
 join order_details
 using (order_id)
 join products
-using (product_id);
+using (product_id)
+group by c.customer_id
 )
+select *
+from top_3
+where de_rank <=3;
 
 --I'm taking too long to figure out these 
+-- No longer the case
